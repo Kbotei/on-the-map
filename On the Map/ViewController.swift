8311d87885c3faba27435a9a0132e4022eaff300
Kbotei <eclipcenet@gmail.com>
2018-12-02 16:46:37 -0600

    Initial Commit

---
+++ On the Map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  On the Map
+//
+//  Created by Kbotei on 12/2/18.
+//  Copyright © 2018 Kbotei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
